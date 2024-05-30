dcb6432d5f8684a8ea00109d9b2e8a725e960326
mohammadnpak <m.nozaripak@gmail.com>
2024-05-30 17:02:00 +0330

    s04 a-b

---
+++ s04/s04.py
@@ -0,0 +1,111 @@
+# a = int(input())
+# b = int(input())
+
+# if a<b:
+#     print("b is grater than a")
+#     print("b is grater than a")
+#     print("b is grater than a")
+#     print("b is grater than a")
+
+# print("rest of program")
+
+
+# a,b = input().split()   #"1 2"
+# a = int(a)
+# b = int(b)
+# print(a+b)
+
+# a, b = map(int, input().split())  # "1 2"
+# a = int(a)
+# b = int(b)
+# print(a+b)
+
+
+# t = int(input())
+# if 100 < t:
+#     print("Stram")
+# elif t<0:
+#     print("Ice")
+# else:
+#     print("Water")
+
+
+
+
+
+
+# a = 0
+# while a<=10:
+#     a+=1
+#     if a ==3:
+#         continue
+#     print(a)
+#     if a==7:
+#         break
+
+# else:
+#     print("inside the else section of loop")
+# print("rest of program")
+
+
+# n=int(input())
+# i=0
+# while i <n:
+#     print("man khoshghlab hastam")
+#     i=i+1
+
+
+
+
+# print("salam",end="")
+# print("mohammad",end="")
+# print("mohammad",end="")
+# print("mohammad",end="")
+# print("mohammad",end="")
+# print("mohammad",end="")
+
+# n = int(input())
+# i=0
+# print("W",end="")
+# while i<n:
+#     print("o",end="")
+#     i+=1
+# print("w!")
+
+
+
+# a = [2,4,3,8,10]
+# s=0
+# for x in a:
+#     s+=x
+# print(s/len(a))
+
+
+# s = "2 3 5 8"
+# a = s.split()
+# print(a)
+# print(type(a))
+# print(a[0])
+
+# a = input().split()
+# b= 0
+# for i in a:
+#     b+=int(i)
+# print(b/len(a))
+
+# s=0
+# for i in range(0,101,7):
+#     # print(i,end=", ")
+#     s+=i
+# print(s)
+
+
+
+# n = int(input())
+# a = 1
+# while a<n:
+#     a*=2
+# print(a//2) 
+
+
+
